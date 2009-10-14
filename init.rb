75536bccf2174168903af2201082b05c8fc7ab56
Eric Davis <edavis@littlestreamsoftware.com>
2009-10-14 14:24:16 -0700

    Added init file for Rails GemPlugin

--- init.rb
+++ init.rb
@@ -1,10 +1,1 @@
-Redmine::Plugin.register :redmine_static_link do
-  name 'Redmine link'
-  author 'Eric Davis of Little Stream Software'
-  description 'Shows a link on the top menu'
-
-  version '0.1.0'
-  
-  menu :top_menu, :example_link, "http://www.example.com"
-end
-
+require File.dirname(__FILE__) + "/rails/init"
